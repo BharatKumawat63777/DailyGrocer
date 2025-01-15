7222aeccf90f65c87176a45b04211e01ec521d50
BharatKumawat63777 <bharatkumawat63777@gmail.com>
2025-01-16 01:13:10 +0530

    Now SomeUpdate

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,14 @@
+import useModel from "../models/useModel.js";
+
+// Add items to user cart
+
+const addToCart = async (req, res) => {};
+
+// remove items from userCart
+
+const removeFromCart = async (req, res) => {};
+
+// fetch user cart data
+const getCart = async (req, res) => {};
+
+export default { addToCart, removeFromCart, getCart };
