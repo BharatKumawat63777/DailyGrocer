6c55fdf453ca6d43b509b42f1bc294386584bb34
BharatKumawat63777 <bharatkumawat63777@gmail.com>
2025-01-26 15:44:02 +0530

    Cloudinary storage image

--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -1,18 +1,31 @@
 import express from "express";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import {
   addFood,
   listFood,
   removefood,
 } from "../controllers/foodcontroller.js";
 import multer from "multer"; //Image storage system
+import { v2 as cloudinary } from "cloudinary";
 
 const foodRouter = express.Router();
 
+// configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 // Image Storage Engine
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}-${file.originalname}`);
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: "uploads",
+      allowed_formats: ["jpeg", "png", "jpg"],
+      public_id: `${Date.now()}-${file.originalname}`,
+    };
   },
 });
 
